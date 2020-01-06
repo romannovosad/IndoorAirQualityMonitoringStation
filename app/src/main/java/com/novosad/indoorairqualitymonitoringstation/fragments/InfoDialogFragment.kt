0a929a0ff56b41d36c35bba17df3a14202127cf2
romannovosad <romannovosad11@gmail.com>
2020-01-06 23:32:22 +0100

    Make code prettier

--- app/src/main/java/com/novosad/indoorairqualitymonitoringstation/fragments/InfoDialogFragment.kt
+++ app/src/main/java/com/novosad/indoorairqualitymonitoringstation/fragments/InfoDialogFragment.kt
@@ -8,7 +8,11 @@ import com.novosad.indoorairqualitymonitoringstation.contstants.Constants
 import kotlinx.android.synthetic.main.info_view_fragment.view.*
 
 class InfoDialogFragment : DialogFragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         // inflate the layout as embedded fragment
         val view = inflater.inflate(R.layout.info_view_fragment, container, false)
         // dismiss dialog on click outside of viewing area
@@ -18,11 +22,39 @@ class InfoDialogFragment : DialogFragment() {
     }
 
     private fun displayInfoTexts(view: View) {
-        view.info_dialog_temperature_description.text = getString(R.string.temperature_description,Constants.TEMPERATURE_THRESHOLD_LOW, Constants.TEMPERATURE_THRESHOLD_MID_LOW, Constants.TEMPERATURE_THRESHOLD_MID_HIGH, Constants.TEMPERATURE_THRESHOLD_HIGH)
-        view.info_dialog_humidity_description.text = getString(R.string.humidity_description,Constants.HUMIDITY_THRESHOLD_LOW, Constants.HUMIDITY_THRESHOLD_MID_LOW, Constants.HUMIDITY_THRESHOLD_MID_HIGH, Constants.HUMIDITY_THRESHOLD_HIGH)
-        view.info_dialog_co2_description.text = getString(R.string.co2_description,Constants.CO2_THRESHOLD_MID_HIGH, Constants.CO2_THRESHOLD_HIGH)
-        view.info_dialog_tvoc_description.text = getString(R.string.tvoc_description,Constants.TVOC_THRESHOLD_MID_HIGH, Constants.TVOC_THRESHOLD_HIGH)
-        view.info_dialog_pm25_description.text = getString(R.string.pm25_description,Constants.PM25_THRESHOLD_MID_HIGH, Constants.PM25_THRESHOLD_HIGH)
-        view.info_dialog_pm10_description.text = getString(R.string.pm10_description,Constants.PM10_THRESHOLD_MID_HIGH, Constants.PM10_THRESHOLD_HIGH)
+        view.info_dialog_temperature_description.text = getString(
+            R.string.temperature_description,
+            Constants.TEMPERATURE_THRESHOLD_LOW,
+            Constants.TEMPERATURE_THRESHOLD_MID_LOW,
+            Constants.TEMPERATURE_THRESHOLD_MID_HIGH,
+            Constants.TEMPERATURE_THRESHOLD_HIGH
+        )
+        view.info_dialog_humidity_description.text = getString(
+            R.string.humidity_description,
+            Constants.HUMIDITY_THRESHOLD_LOW,
+            Constants.HUMIDITY_THRESHOLD_MID_LOW,
+            Constants.HUMIDITY_THRESHOLD_MID_HIGH,
+            Constants.HUMIDITY_THRESHOLD_HIGH
+        )
+        view.info_dialog_co2_description.text = getString(
+            R.string.co2_description,
+            Constants.CO2_THRESHOLD_MID_HIGH,
+            Constants.CO2_THRESHOLD_HIGH
+        )
+        view.info_dialog_tvoc_description.text = getString(
+            R.string.tvoc_description,
+            Constants.TVOC_THRESHOLD_MID_HIGH,
+            Constants.TVOC_THRESHOLD_HIGH
+        )
+        view.info_dialog_pm25_description.text = getString(
+            R.string.pm25_description,
+            Constants.PM25_THRESHOLD_MID_HIGH,
+            Constants.PM25_THRESHOLD_HIGH
+        )
+        view.info_dialog_pm10_description.text = getString(
+            R.string.pm10_description,
+            Constants.PM10_THRESHOLD_MID_HIGH,
+            Constants.PM10_THRESHOLD_HIGH
+        )
     }
 }
